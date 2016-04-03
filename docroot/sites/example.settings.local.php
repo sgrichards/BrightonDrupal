3fd8c479123a8a142bc8fce0d66f6df3389544ce
Steve Richards <srichards@inviqa.com>
2016-04-03 11:40:55 +0100

    removing local dev settings files from vcs - moved to example files

--- docroot/sites/example.settings.local.php
+++ docroot/sites/example.settings.local.php
@@ -14,6 +14,17 @@
  * at the bottom of 'sites/example.com/settings.php'.
  */
 
+$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'drupal',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+
 /**
  * Assertions.
  *
